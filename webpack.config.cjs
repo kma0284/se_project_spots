2534cdd74a08d8d79717f16c67f5688ebbf08327
kma0284 <kma0284@gmail.com>
2026-03-19 18:38:35 -0400

    webpack config

---
+++ webpack.config.cjs
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/pages/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|webp|gif|woff2?|eot|ttf|otf|ico)$/i, // added .ico
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: "./src/images/spots-images/favicon.ico", // favicon handled automatically
+    }),
+    new MiniCssExtractPlugin({ filename: "styles.css" }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 8080,
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  target: ["web", "es5"],
+};
